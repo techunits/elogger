2839cbd9f20a756e458a9a337ea6ee60c1eed1fe
Sougata Pal <skall.paul@techunits.com>
2015-01-01 17:46:37 +0530

    updated logger with input processing

--- logger.js
+++ logger.js
@@ -18,14 +18,23 @@ exports = module.exports = function(type) {
 
 exports.debug = function(str, flag) {
 	if(null == flag || true === flag) {
-		console.log('DEBUG: ' + "\t" + new Date() + "\t" + str);
+		console.log('DEBUG: ' + "\t" + new Date() + "\t" + processInput(str));
 	}
 };
 
 exports.info = function(str) {
-	console.log('INFO: ' + "\t" + new Date() + "\t" + str);
+	console.log('INFO: ' + "\t" + new Date() + "\t" + processInput(str));
 };
 
 exports.error = function(str) {
-	console.log("\033[31m" + 'ERROR: ' + "\t" + new Date() + "\t" + str + "\033[0m");
+	console.log("\033[31m" + 'ERROR: ' + "\t" + new Date() + "\t" + processInput(str) + "\033[0m");
+};
+
+function processInput(param) {
+	if ('string' == typeof param) {
+		return param;
+	}
+	else {
+		return JSON.stringify(param);
+	}
 };
